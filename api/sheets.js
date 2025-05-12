de8045707fe56d76272ba275495839b247dc91be
ruri1307 <161279924+ruri1307@users.noreply.github.com>
2025-05-12 12:45:30 +0800

    dynamic dropdown values

--- api/sheets.js
+++ api/sheets.js
@@ -2,7 +2,6 @@
 
 export default async function handler(req, res) {
     try {
-      // 1) Proxy your Apps Script web app URL
       const response = await fetch(
         'https://script.google.com/macros/s/AKfycbx_85W2Kdj0y5SqS9ykBjmyHvUGEl-vKj4NbmRTwNHEHNvWOrwhzrEvZUb14B_RFXfCjw/exec'
       );
@@ -10,10 +9,8 @@ export default async function handler(req, res) {
         throw new Error(`Upstream returned ${response.status}`);
       }
   
-      // 2) Parse JSON
       const data = await response.json();
   
-      // 3) Return it to the browser
       return res.status(200).json(data);
     }
     catch (err) {
--- public/script.js
+++ public/script.js
@@ -5,3 +5,14 @@ fetch('/api/sheets')
   .then(sheetData => {
     data = sheetData;
   });
+
+const list_option = document.createElement("option");
+const list = document.getElementById("name-list");
+
+for (var i = 0; i < data.length; i++) {
+  for (var j = 0; j < data[i].length; i++){
+    const node = document.createTextNode(data[i][j].Name);
+    list_option.appendChild(node);
+  }
+}
+list.appendChild(list_option);
