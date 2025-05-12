c45200bd27825e690615f2153916af91fa86c282
ruri1307 <161279924+ruri1307@users.noreply.github.com>
2025-05-12 13:14:17 +0800

    dynamic dropdown values 1.8

--- public/script.js
+++ public/script.js
@@ -4,19 +4,19 @@ fetch('/api/sheets')
   .then(res => res.json())
   .then(sheetData => {
     data = sheetData;
+
+    const list = document.getElementById("name-list");
+    const frag = document.createDocumentFragment();
+
+    console.log(data);
+
+    data.flat().forEach(item => {
+      const option = document.createElement("option");
+      option.value = item.Name;
+      frag.appendChild(option);
+    });
+
+    list.appendChild(frag);
   });
 
-const list = document.getElementById("name-list");
-const frag = document.createDocumentFragment();
-
-console.log(data);
-
-data.flat().forEach(item => {
-  const option = document.createElement("option");
-  option.value = item.Name;
-  frag.appendChild(option);
-});
-
-list.appendChild(frag);
-
-//dynamic dropdown valyes
+//dynamic dropdown values
